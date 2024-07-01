136a1905077e3d764e9f2d9ebb8e20eb18a5af66
HETVI1405 <phetvi1405@gmail.com>
2024-07-01 11:44:34 +0530

    Add files via upload

---
+++ tarnery.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int x;
+	printf("enter a value x :");
+	scanf("%d,x");
+	(9<5)?printf("minimum number is 9"):printf("minimum number is 5"); 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
